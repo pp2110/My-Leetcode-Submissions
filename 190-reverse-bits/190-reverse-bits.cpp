a7002790cf54eb2b595a3d464d0bddf649d5002c
pp2110 <81473333+pp2110@users.noreply.github.com>
2022-09-20 20:33:31 +0530

    Time: 6 ms (31.43%), Space: 6 MB (11.14%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+uint32_t reverseBits(uint32_t n) {
+    int ans =0;
+    for(int i=31;i>=0;i--) {
+        ans|= (n & 1)<< i;
+        n= n>>1;
+    }
+    return ans;
+}
+};
