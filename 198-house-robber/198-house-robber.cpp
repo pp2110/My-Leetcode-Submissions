dd616df2ad13f418e84ffc6a5ee088abc52f9c71
pp2110 <81473333+pp2110@users.noreply.github.com>
2022-09-23 16:29:52 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (49.69%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -19,8 +19,25 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         
-        vector<int> dp(n, -1);
+        int prev = nums[0];
+        int prev2 = 0;
         
-        return helper(n-1, nums, dp);
+        for(int i=1; i<n; i++){
+            
+            int take = nums[i];
+            if(i>1)
+                take+=prev2;
+            
+            int notTake = prev;
+            
+            int curri = max(take, notTake);
+            
+            prev2 = prev;
+            prev = curri;
+            
+        }
+        
+        return prev;
+        
     }
 };
