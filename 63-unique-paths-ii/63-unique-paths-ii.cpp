b0b9d4ab94e0e4a3144ddb0bc1b0f587f036e4dc
pp2110 <81473333+pp2110@users.noreply.github.com>
2022-09-25 16:56:49 +0530

    Time: 3 ms (86.15%), Space: 7.8 MB (45.62%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -31,6 +31,31 @@ public:
         
         vector<vector<int>> dp(n, vector<int>(m, -1));
         
-        return helper(n-1, m-1, dp, obstacleGrid);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(i>=0 and j>=0 and obstacleGrid[i][j] == 1)
+                    dp[i][j] = 0;
+                
+                else if(i==0 and j==0)
+                    dp[i][j] = 1;
+                
+                else{
+                    int up = 0;
+                    int left = 0;
+                    
+                    if(i>0)
+                        up = dp[i-1][j];
+                    
+                    if(j>0)
+                        left = dp[i][j-1];
+                    
+                    dp[i][j] = left+up;
+                }
+            }
+        }
+        
+        
+        return dp[n-1][m-1];
+        // return helper(n-1, m-1, dp, obstacleGrid);
     }
 };
