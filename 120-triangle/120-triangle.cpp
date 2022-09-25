4f6a0c4856a190e3955babc3ed0a1a7949562778
pp2110 <81473333+pp2110@users.noreply.github.com>
2022-09-25 18:30:14 +0530

    Time: 12 ms (43.97%), Space: 9 MB (32.66%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -23,7 +23,20 @@ public:
         
         vector<vector<int>> dp(n, vector<int>(n, -1));
         
-        return helper(0, 0, n, dp, triangle);
+        for(int i=0; i<n; i++){
+                dp[n-1][i] = triangle[n-1][i];
+        }
+        
+        for(int i = n-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                int down = dp[i+1][j] + triangle[i][j];
+                int diag = dp[i+1][j+1] + triangle[i][j];
+                
+                dp[i][j] = min(down, diag);
+            }
+        }
+        
+        return dp[0][0];
         
     }
 };
