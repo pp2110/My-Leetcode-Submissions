df01055e2cc2d821ad0f1c7c62c88d303570f527
pp2110 <81473333+pp2110@users.noreply.github.com>
2023-03-04 11:53:41 +0530

    Added solution - LeetHub

---
+++ Euler Circuit in an Undirected Graph - GFG/euler-circuit-in-an-undirected-graph.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+	bool isEularCircuitExist(int V, vector<int>adj[]){
+	    // Code here
+	    int count = 0, odd = 0;
+	    
+	    for(int i=0; i<V; i++){
+	        for(auto iter : adj[i]){
+	            count++;
+	        }
+	        if(count % 2 == 1){
+	            odd++;
+	        }
+	    }
+	    
+	    if(odd == 0)
+	        return 1;
+	        
+	        return 0;
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		bool ans = obj.isEularCircuitExist(V, adj);
+		if(ans)
+			cout << "1\n";
+		else cout << "0\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
